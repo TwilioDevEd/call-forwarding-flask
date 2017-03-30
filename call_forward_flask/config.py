b0e33d7784bf2194978b98183dac69f85f1fcea2
Kat King <katkingcodes@gmail.com>
2017-03-30 14:46:47 -0500

    Test including Twilio keys in app config
    * Includes: remove .env_example

--- call_forward_flask/config.py
+++ call_forward_flask/config.py
@@ -1,5 +1,10 @@
 import os
 
+from secrets import(
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN,
+)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -14,6 +19,10 @@ class DevelopmentConfig(DefaultConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = ('sqlite:///' +
                                os.path.join(basedir, 'dev.sqlite'))
+    # Twilio API Key
+    # Found at your Twilio dashboard https://www.twilio.com/console
+    TWILIO_ACCOUNT_SID = TWILIO_ACCOUNT_SID
+    TWILIO_AUTH_TOKEN = TWILIO_AUTH_TOKEN
 
 
 class TestConfig(DefaultConfig):
